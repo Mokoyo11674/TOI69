da8a6a7aa0203036b03be92f0f0ec6464a8e0c40
Mokoyo11674 <11674@triamudomsouth.ac.th>
2026-05-16 16:11:52 +0700

    33

--- A1033.cpp
+++ A1033.cpp
@@ -1,25 +1,25 @@
 #include <stdio.h>
-
+#include <ctype.h> 
 int main() {
-    int n;
-    int count = 0; 
-    char ch;
+    int n, count = 0;
+    char c;
 
     
-    if (scanf("%d", &n) != 1) {
-        return 0;
-    }
+    if (scanf("%d", &n) != 1) return 0;
 
-    
+   
     for (int i = 0; i < n; i++) {
         
-        scanf(" %c", &ch);
-
+        scanf(" %c", &c);
+        
+        
+        c = toupper(c); 
+        
        
-        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-            count++; 
+        if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+            count++;
+        }
     }
-
 
     printf("%d\n", count);
 
